ea030cb951b390ab06cc60b626d28608232015d0
Claire Isabelle <claire.bourdale@gmail.com>
2017-02-16 13:37:33 +0100

    Mise en place template

---
+++ src/ArrowebBundle/ArrowebBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ArrowebBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ArrowebBundle extends Bundle
+{
+}
---
+++ src/ArrowebBundle/Controller/PagesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ArrowebBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+
+class PagesController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('ArrowebBundle:pages:index.html.twig');
+    }
+}
