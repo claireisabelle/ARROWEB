dd3a2681ec29680c7c2a06dabe691c0493c80a91
Claire Isabelle <claire.bourdale@gmail.com>
2017-05-04 17:09:59 +0200

    Portfolio - creation Entite Reference

--- src/ArrowebBundle/Controller/PortfolioController.php
+++ src/ArrowebBundle/Controller/PortfolioController.php
@@ -5,6 +5,10 @@ namespace ArrowebBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+use Symfony\Component\HttpFoundation\Request;
+
+use ArrowebBundle\Entity\Reference;
+
 
 class PortfolioController extends Controller
 {
@@ -15,6 +19,47 @@ class PortfolioController extends Controller
     {
         return $this->render('ArrowebBundle:portfolio:portfolio.html.twig');
     }
+
+    /**
+     * @Route("/gestion", name="dashboard")
+     */
+    public function dashboardAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        // Récupération de toutes les références
+        $listeReferences = $em->getRepository('ArrowebBundle:Reference')->getReferences();
+
+        return $this->render('ArrowebBundle:portfolio:dashboard.html.twig', array('listeReferences' => $listeReferences));
+    }
+
+    /**
+     * @Route("/gestion/ajouter", name="ajouter-reference")
+     */
+    public function ajouterAction(Request $request)
+    {
+        
+        $reference = new Reference();
+        $reference->setTitre('Le Trail de lAngoisse');
+        $reference->setDescription('Le site Internet officile de Hélène');
+        $reference->setStatut('En ligne');
+        $reference->setUrl('www.traildelangoisse.com');
+        $reference->setAnnee('2016');
+
+        // Récupération de l'Entity Manager
+        $em = $this->getDoctrine()->getManager();
+
+        // Persistement de la Référence
+        $em->persist($reference);
+        $em->flush();
+
+
+        $request->getSession()->getFlashBag()->add('success', 'La référence a bien été ajoutée.');
+
+        return $this->redirectToRoute('dashboard');
+    }
+
+
 
 
 }
---
+++ src/ArrowebBundle/Entity/Reference.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace ArrowebBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Reference
+ *
+ * @ORM\Table(name="arroweb_reference")
+ * @ORM\Entity(repositoryClass="ArrowebBundle\Repository\ReferenceRepository")
+ */
+class Reference
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="statut", type="string", length=255)
+     */
+    private $statut;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=255)
+     */
+    private $url;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="annee", type="string", length=255)
+     */
+    private $annee;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     *
+     * @return Reference
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Reference
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set statut
+     *
+     * @param string $statut
+     *
+     * @return Reference
+     */
+    public function setStatut($statut)
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+
+    /**
+     * Get statut
+     *
+     * @return string
+     */
+    public function getStatut()
+    {
+        return $this->statut;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     *
+     * @return Reference
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set annee
+     *
+     * @param string $annee
+     *
+     * @return Reference
+     */
+    public function setAnnee($annee)
+    {
+        $this->annee = $annee;
+
+        return $this;
+    }
+
+    /**
+     * Get annee
+     *
+     * @return string
+     */
+    public function getAnnee()
+    {
+        return $this->annee;
+    }
+}
+
